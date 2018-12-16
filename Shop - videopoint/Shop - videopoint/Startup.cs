753b134f7bb0c95cbc8f36828f374d2f58775a78
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2018-12-16 07:21:36 +0100

    Utworzenie projektu

---
+++ Shop - videopoint/Shop - videopoint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shop___videopoint.Startup))]
+namespace Shop___videopoint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
