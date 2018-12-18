02e3b4ad069c263ecb5196768e9d7bd5b44d416f
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2018-12-18 09:58:50 +0100

    Powtórzenie ostatniego commita dla nowego kontrolera Test2Controller, oraz widoku Hello2

---
+++ Shop - videopoint/Shop - videopoint/Controllers/Test2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop___videopoint.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        // GET: Test2
+        public ActionResult Index()
+        {
+            return Content("Witaj świecie2");
+        }
+
+        public ActionResult Hello2(string id2)
+        {
+            return View();
+        }
+    }
+}
