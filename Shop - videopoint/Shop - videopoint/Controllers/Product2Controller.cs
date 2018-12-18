df923a593685b67b2dcb9225d8f0fd6e498dba82
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2018-12-18 11:45:07 +0100

    -Powtórzenie dla kontrolera Product2, Modelu Product2, oraz widoku Display kontrolera Product2

--- Shop - videopoint/Shop - videopoint/Controllers/Product2Controller.cs
+++ Shop - videopoint/Shop - videopoint/Controllers/Product2Controller.cs
@@ -7,18 +7,31 @@ using System.Web.Mvc;
 
 namespace Shop___videopoint.Controllers
 {
-    public class Prouct2Controller : Controller
+    public class Product2Controller : Controller
     {
         // GET: Prouct2
-        public ActionResult Index()
+        public void Index()
+        {
+           
+        }
+        public ActionResult Display(int id)
         {
             Product2 product = new Product2
             {
+                id = 1,
                 Name = "ASP.NET MVC for beginners",
                 Description = "Kurs dla początkujących",
                 Price = 100
             };
-            return View(product);
+            if (id == product.id)
+            {
+                return View(product);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
         }
     }
 }
