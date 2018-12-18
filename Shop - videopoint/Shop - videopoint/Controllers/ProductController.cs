e9bf139a843b9dfdf6e092d2e4594b6dae8b30b1
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2018-12-18 11:29:47 +0100

    -Usunięcie Kontrolerów Test i Test2 -Usunięcie widoków Hello i Hello2 -Dodanie modelu Product -Dodanie kontrolera Product -Utworzenie widoku Index dla kontrolera Product -Przekazanie modelu do widoku poprzez akcje Index -Powtórzenie dla modelu Product2, kontrolera Product2, widoku Index, oraz przekazanie danych do widoku

---
+++ Shop - videopoint/Shop - videopoint/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Shop___videopoint.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop___videopoint.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Prouct
+        public ActionResult Index()
+        {
+            Product product = new Product();
+            product.Name = "Książka ASP.NET MVC";
+            product.Description = "Znajdziemy tutaj bardzo dużo wiedzy";
+            product.Price = 100;
+            return View(product);
+        }
+    }
+}
---
+++ Shop - videopoint/Shop - videopoint/Controllers/Prouct2Controller.cs
@@ -0,0 +1,24 @@
+﻿using Shop___videopoint.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop___videopoint.Controllers
+{
+    public class Prouct2Controller : Controller
+    {
+        // GET: Prouct2
+        public ActionResult Index()
+        {
+            Product2 product = new Product2
+            {
+                Name = "ASP.NET MVC for beginners",
+                Description = "Kurs dla początkujących",
+                Price = 100
+            };
+            return View(product);
+        }
+    }
+}
