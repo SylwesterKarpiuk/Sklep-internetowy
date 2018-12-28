ccda12bd94132cb08ce6d7a35a9947f5e849a154
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2018-12-28 11:35:13 +0100

    - Przypisanie użytkownika do tworzonej przez niego recenzji - Ograniczenie edycji komentarza tylko dla użytkownika, który go stworzył

--- Shop - videopoint/Shop - videopoint/Controllers/ReviewsController.cs
+++ Shop - videopoint/Shop - videopoint/Controllers/ReviewsController.cs
@@ -29,6 +29,7 @@ namespace Shop___videopoint.Controllers
         {
             if (ModelState.IsValid)
             {
+                model.Name = User.Identity.Name;
                 _db.Reviews.Add(model);
                 _db.SaveChanges();
                 return RedirectToAction("index", new { id = model.ProductId });
@@ -38,24 +39,30 @@ namespace Shop___videopoint.Controllers
         public ActionResult Edit(int id)
         {
             var review = _db.Reviews.Find(id);
-            if (review != null)
+
+            if (review.Name == User.Identity.Name)
             {
-                return View(review);
+                if (review != null)
+                {
+                    return View(review);
+                }
             }
-            else
-            {
-                return RedirectToAction("Index", "Products");
-            }
+            return RedirectToAction("Index", "Products");
+
         }
         [HttpPost]
         public ActionResult Edit([Bind(Exclude="Name")]Review model)
         {
-            if (ModelState.IsValid)
+            if (model.Name == User.Identity.Name)
             {
-                _db.Entry(model).State = System.Data.Entity.EntityState.Modified;
-                _db.SaveChanges();
-                return RedirectToAction("Index", new { id = model.ProductId });
+                if (ModelState.IsValid)
+                {
+                    _db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", new { id = model.ProductId });
+                }
             }
+            
             return View(model);
             
         }
